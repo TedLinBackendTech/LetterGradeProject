4b360d8ca304c7d1b2921d89d5dd3f98bc3d9cc4
TedLin <t109598073@ntut.org.tw>
2020-12-29 14:24:59 +0800

    [feature] add whole test

--- src/LGTest.java
+++ src/LGTest.java
@@ -4,27 +4,27 @@ import org.junit.jupiter.api.Test;
 
 class LGTest {
 
-//	@Test
-//	void testnormalcase() {
-//		
-//		letterGrade LG = new  letterGrade();
-//		
-//		assertEquals('A', LG.getScoreLetter(100));
-//		assertEquals('A', LG.getScoreLetter(95));
-//		assertEquals('A', LG.getScoreLetter(90));
-//		assertEquals('B', LG.getScoreLetter(85));
-//		assertEquals('B', LG.getScoreLetter(80));
-//		assertEquals('C', LG.getScoreLetter(75));
-//		assertEquals('C', LG.getScoreLetter(70));
-//		assertEquals('D', LG.getScoreLetter(65));
-//		assertEquals('D', LG.getScoreLetter(60));
-//		assertEquals('F', LG.getScoreLetter(55));
-//		assertEquals('F', LG.getScoreLetter(50));
-//		assertEquals('F', LG.getScoreLetter(0));
-//		assertEquals('X', LG.getScoreLetter(-1));		
-//		assertEquals('X', LG.getScoreLetter(101));
-//
-//	}
+	@Test
+	void testnormalcase() {
+		
+		letterGrade LG = new  letterGrade();
+		
+		assertEquals('A', LG.getScoreLetter(100));
+		assertEquals('A', LG.getScoreLetter(95));
+		assertEquals('A', LG.getScoreLetter(90));
+		assertEquals('B', LG.getScoreLetter(85));
+		assertEquals('B', LG.getScoreLetter(80));
+		assertEquals('C', LG.getScoreLetter(75));
+		assertEquals('C', LG.getScoreLetter(70));
+		assertEquals('D', LG.getScoreLetter(65));
+		assertEquals('D', LG.getScoreLetter(60));
+		assertEquals('F', LG.getScoreLetter(55));
+		assertEquals('F', LG.getScoreLetter(50));
+		assertEquals('F', LG.getScoreLetter(0));
+		assertEquals('X', LG.getScoreLetter(-1));		
+		assertEquals('X', LG.getScoreLetter(101));
+
+	}
 	
 	@Test
 	void testboundarycase() {
